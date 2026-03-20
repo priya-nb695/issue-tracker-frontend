ff54ddcd28fd475bdfabf944eccac0e1a134442e
priyanb <priyanb250@gmail.com>
2026-03-20 21:11:36 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React,{ useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { getIssues } from "./services/issueService";
 
 function App() {
-  const [issues,setIssues] = useState([]);
- 
-  useEffect(()=>{
+  const [issues, setIssues] = useState([]);
+
+  useEffect(() => {
     fetchIssues();
-  },[]);
-  
+  }, []);
+
   const fetchIssues = async () => {
-    const issues = await getIssues ();
-    setIssues(issues);
-  }
+    const data = await getIssues();
+    setIssues(data);
+  };
 
   return (
-    <div >
+    <div>
       <h1>Issue Tracker</h1>
+
       {issues?.map((issue) => (
-        <div key={issue.id}>
+        <div key={issue._id}>
           <h3>{issue.title}</h3>
           <p>{issue.description}</p>
           <p>Status: {issue.status}</p>
           <p>Priority: {issue.priority}</p>
+          <hr />
         </div>
       ))}
     </div>
--- src/services/issueService.js
+++ src/services/issueService.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const API_URL = "http://localhost:5000/issues";
 
-export const getIssues = async (params) => {
-    const response = axios.get(API_URL);
-    return response.data ;
-    
+export const getIssues = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
 };
