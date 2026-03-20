79d6a0aae5d1bfb56cd929454a59deb81b739239
priyanb <priyanb250@gmail.com>
2026-03-20 20:56:01 +0530

    strated app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-
+import React,{ useEffect, useState} from "react";
+import { getIssues } from "./services/issueService";
 
 function App() {
+  const [issues,setIssues] = useState([]);
+ 
+  useEffect(()=>{
+    fetchIssues();
+  },[]);
+  
+  const fetchIssues = async () => {
+    const issues = await getIssues ();
+    setIssues(issues);
+  }
+
   return (
     <div className="App">
       <h1>Issue Tracker</h1>
